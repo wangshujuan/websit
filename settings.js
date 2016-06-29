4cce38a069c02b92bbc19aa89f6ed4734480e8de
wangshujuan <1518987721@qq.com>
2016-06-29 11:04:41 +0800

    settings.js

---
+++ settings.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+var mysql = require('mysql');
+
+
+
+var settings={};
+settings.db= ({
+    host: '220.165.246.91',
+    user: 'stutest',
+    password: '1q2w3eStuTest',
+    database: 'info',
+    port: 3301
+});
+
+//function  settings() {};
+module.exports=settings;
+
+
